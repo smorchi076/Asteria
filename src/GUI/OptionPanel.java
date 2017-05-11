67c9b5799a3e6cc0b186052539c884353715a147
smorchi076 <smorchi076@PR16.hhs.win.fuhsd.org>
2017-05-11 09:22:35 -0700

    play button

--- src/GUI/OptionPanel.java
+++ src/GUI/OptionPanel.java
@@ -17,27 +17,61 @@ public class OptionPanel extends JPanel implements ActionListener {
 	Asteria game;
 	
 	public OptionPanel(Asteria game) {
+		
 		this.game = game;
 		JButton button = new JButton("Press me!");
 		button.addActionListener(this);
 		add(button);
 		
-		
-		BufferedImage buttonIcon = null;
-		try {
-			buttonIcon = ImageIO.read(new File("myImage.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    button = new JButton(new ImageIcon(buttonIcon));
-	    button.setBorderPainted(false);
-	    button.setFocusPainted(false);
-	    button.setContentAreaFilled(false);
+		Icon warnIcon = new ImageIcon("resources/play.png");
+	    JButton button2 = new JButton(warnIcon);
+	    add(button2);
+	    button2.setContentAreaFilled(false);
+	    
+	 
+	   
+	   
+	    
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		game.changePanel("2");
 	}
 	
+	
+	public void paintComponent(Graphics page)
+	{
+		
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("resources/background.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    super.paintComponent(page);
+
+	    int h = img.getHeight(null);
+	    int w = img.getWidth(null);
+
+	    // Scale Horizontally:
+	    if ( w > this.getWidth() )
+	    {
+	        img = (BufferedImage) img.getScaledInstance( getWidth(), -1, Image.SCALE_DEFAULT );
+	        h = img.getHeight(null);
+	    }
+
+	    // Scale Vertically:
+	    if ( h > this.getHeight() )
+	    {
+	        img = (BufferedImage) img.getScaledInstance( -1, getHeight(), Image.SCALE_DEFAULT );
+	    }
+
+	    // Center Images
+	    int x = (getWidth() - img.getWidth(null)) / 2;
+	    int y = (getHeight() - img.getHeight(null)) / 2;
+
+	    // Draw it
+	    page.drawImage( img, x, y, null );
+	}
 }
