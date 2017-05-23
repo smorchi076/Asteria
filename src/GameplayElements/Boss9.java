45ee4a1222c22466b1e92a54e2c54c77e77e9d5e
sahilmorchi <sahil.morchi@gmail.com>
2017-05-22 19:41:35 -0700

    boss 9 done, move on to boss 10

--- src/GameplayElements/Boss9.java
+++ src/GameplayElements/Boss9.java
@@ -38,7 +38,7 @@ public class Boss9 extends MovingImage {
 		if(shootClock==0){
 			for(int i=0; i<blasts.length; i++){
 				if(blasts[i]==null || blasts[i].isFizzled()){
-					blasts[i] = new Projectile((int)(getCenterX())-10, (int)(getCenterY()), super.getDirection() + Math.PI, "resources/Bullet.png",30,40,30);
+					blasts[i] = new Projectile((int)(getCenterX())-10, (int)(getCenterY()), super.getDirection() + 2*Math.PI, "resources/Bullet.png",30,40,30);
 					shootClock = 150;
 					break;
 				}
